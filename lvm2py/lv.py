b6ddeff70dd2f1f9b72df31fab164e2958f751d1
Court <campbco@gmail.com>
2018-10-22 09:17:07 -0500

    Update lv.py
    Modified for relative import.

--- lvm2py/lv.py
+++ lvm2py/lv.py
@@ -13,9 +13,9 @@
 #You should have received a copy of the GNU General Public License
 #along with lvm2py. If not, see <http://www.gnu.org/licenses/>.
 
-from conversion import *
-from exception import *
-from util import *
+from .conversion import *
+from .exception import *
+from .util import *
 
 
 class LogicalVolume(object):
